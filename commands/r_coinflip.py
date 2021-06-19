764c11c0823a775dd437d19c9f7832d414379881
Enes <eneskurbetoglu1@gmail.com>
2021-06-20 00:35:31 +0300

    grammar

--- commands/r_coinflip.py
+++ commands/r_coinflip.py
@@ -7,9 +7,9 @@ determine_flip = [1, 0]
 
 async def commands_coinflip(ctx):
     if random.choice(determine_flip) == 1:
-        embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} Flipped a coin, and got **Heads**, I suppose!")
+        embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} flipped a coin, and got **Heads**, I suppose!")
         await ctx.send(embed=embed)
 
     else:
-        embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} Flipped a coin, and got **Tails**, I suppose!")
+        embed = discord.Embed(title="Coinflip", description=f"{ctx.author.mention} flipped a coin, and got **Tails**, I suppose!")
         await ctx.send(embed=embed)
