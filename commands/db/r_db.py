1f48ba6b9cba01efd89501d5b3fa98c744ea13bd
Enes <eneskurbetoglu1@gmail.com>
2021-10-21 10:32:59 +0300

    added kaguya-sama and oshi no ko translation notifications as well
    parsed from https://guya.moe

--- commands/db/r_db.py
+++ commands/db/r_db.py
@@ -18,7 +18,14 @@ db_flips = client.flips
 
 flips = db_flips.data
 
+# re zero
 channels = db_channels.data
+
+# kaguya-sama
+channels_kaguya = db_channels.data_kaguya
+
+# oshi no ko
+channels_onk = db_channels.data_onk
 
 # sends the latest english translated chapter
 async def commands_latest_chapter(ctx):
@@ -39,7 +46,7 @@ async def commands_latest_chapter(ctx):
   title = most_recent_post.text
   await ctx.send(f'Latest translated chapter is {title}, I suppose!\n{latest_chapter_translated_link}')
 
-# add channel
+# add channel to re zero notification list
 async def commands_add_channel(ctx):
   channel_entry = {
     'id': ctx.channel.id,
@@ -50,7 +57,29 @@ async def commands_add_channel(ctx):
   channels.insert_one(channel_entry)
   await ctx.send("This text channel will receive notifications, I suppose!")
   
-# remove channel
+# add channel to kaguya-sama notification list
+async def commands_add_channel_kaguya(ctx):
+  channel_entry = {
+    'id': ctx.channel.id,
+  }
+  if channels_kaguya.count_documents(channel_entry, limit = 1) != 0:
+        await ctx.send("This text channel is already on the receiver list, in fact!")
+        return
+  channels_kaguya.insert_one(channel_entry)
+  await ctx.send("This text channel will receive notifications, I suppose!")
+  
+# add channel to oshi no ko notification list
+async def commands_add_channel_onk(ctx):
+  channel_entry = {
+    'id': ctx.channel.id,
+  }
+  if channels_onk.count_documents(channel_entry, limit = 1) != 0:
+        await ctx.send("This text channel is already on the receiver list, in fact!")
+        return
+  channels_onk.insert_one(channel_entry)
+  await ctx.send("This text channel will receive notifications, I suppose!")
+  
+# remove channel from re zero notification list
 async def commands_remove_channel(ctx):
   channel_entry = {
     'id': ctx.channel.id,
@@ -61,6 +90,28 @@ async def commands_remove_channel(ctx):
   channels.find_one_and_delete(channel_entry)
   await ctx.send("This text channel will no longer receive notifications, I suppose!")
   
+# remove channel from kaguya-sama notification list
+async def commands_remove_channel_kaguya(ctx):
+  channel_entry = {
+    'id': ctx.channel.id,
+  }
+  if channels_kaguya.count_documents(channel_entry, limit = 1) == 0:
+        await ctx.send("This text channel is not on the receiver list, in fact!")
+        return
+  channels_kaguya.find_one_and_delete(channel_entry)
+  await ctx.send("This text channel will no longer receive notifications, I suppose!")
+  
+# remove channel from oshi no ko notification list
+async def commands_remove_channel_onk(ctx):
+  channel_entry = {
+    'id': ctx.channel.id,
+  }
+  if channels_onk.count_documents(channel_entry, limit = 1) == 0:
+        await ctx.send("This text channel is not on the receiver list, in fact!")
+        return
+  channels_onk.find_one_and_delete(channel_entry)
+  await ctx.send("This text channel will no longer receive notifications, I suppose!")
+  
 # task that removes non existing(deleted) channels every 10 seconds
 async def tasks_filter_channels(bot):
   for channel in channels.find():
@@ -69,13 +120,30 @@ async def tasks_filter_channels(bot):
         'id': channel['id'],
       }
       channels.find_one_and_delete(channel_entry)
+  for channel in channels_kaguya.find():
+    if not bot.get_channel((channel['id'])):
+      channel_entry = {
+        'id': channel['id'],
+      }
+      channels_kaguya.find_one_and_delete(channel_entry)
+  for channel in channels_onk.find():
+    if not bot.get_channel((channel['id'])):
+      channel_entry = {
+        'id': channel['id'],
+      }
+      channels_onk.find_one_and_delete(channel_entry)
 
 # task that checks chapter every 10 seconds
 async def tasks_check_chapter(bot):
   page = requests.get('https://witchculttranslation.com/arc-7/')
+  page_kaguya = requests.get('https://guya.moe/read/manga/Kaguya-Wants-To-Be-Confessed-To/')
+  page_onk = requests.get('https://guya.moe/read/manga/Oshi-no-Ko/')
 
   soup = BeautifulSoup(page.content, 'html.parser')
+  soup_kaguya = BeautifulSoup(page_kaguya.content, 'html.parser')
+  soup_onk = BeautifulSoup(page_onk.content, 'html.parser')
 
+  # web scraping for re zero
   most_recent_post = soup.find_all('h3', 'rpwe-title')[0]
 
   post_link = most_recent_post.find('a')
@@ -107,6 +175,69 @@ async def tasks_check_chapter(bot):
       for channel in channels.find():
           if bot.get_channel((channel['id'])):
             await (bot.get_channel(int(channel['id']))).send(f'{most_recent_post} has been translated {time_posted}.\n{latest_chapter_translated_link}, I suppose!')
+          
+  # web scraping for kaguya-sama
+  most_recent_kaguya_chapter = soup_kaguya.find_all('td', 'chapter-title')[0]
+  
+  kaguya_chapter_link = most_recent_kaguya_chapter.find('a')
+  
+  try:
+      if 'href' in post_link.attrs:
+        kaguya_chapter_anchor = 'https://guya.moe'
+        kaguya_chapter_anchor += kaguya_chapter_link.get('href')
+  except:
+      pass
+  
+  most_recent_kaguya_chapter_title = kaguya_chapter_link.text
+  
+  most_recent_kaguya_chapter_array = most_recent_kaguya_chapter_title.split()
+  
+  most_recent_kaguya_chapter_str = ""
+  
+  for i in range(0, len(most_recent_kaguya_chapter_array)):
+    most_recent_kaguya_chapter_str += most_recent_kaguya_chapter_array[i] + " "
+    
+  most_recent_kaguya_chapter_str = most_recent_kaguya_chapter_str.strip()
+  
+  last_kaguya_chapter = db_chapter.data_kaguya.find_one()
+  
+  if last_kaguya_chapter['title'] != most_recent_kaguya_chapter_str:
+      db_chapter.data_kaguya.find_one_and_update({'title':str(last_kaguya_chapter['title'])}, { '$set': { "title" : most_recent_kaguya_chapter_str} })
+      for channel in channels_kaguya.find():
+          if bot.get_channel((channel['id'])):
+            await (bot.get_channel(int(channel['id']))).send(f'{most_recent_kaguya_chapter_str} has been translated.\n{kaguya_chapter_anchor}, I suppose!')
+            
+  # web scraping for oshi no ko
+  most_recent_onk_chapter = soup_onk.find_all('td', 'chapter-title')[0]
+  
+  onk_chapter_link = most_recent_onk_chapter.find('a')
+  
+  try:
+      if 'href' in post_link.attrs:
+        onk_chapter_anchor = 'https://guya.moe'
+        onk_chapter_anchor += onk_chapter_link.get('href')
+  except:
+      pass
+  
+  most_recent_onk_chapter_title = onk_chapter_link.text
+  
+  most_recent_onk_chapter_array = most_recent_onk_chapter_title.split()
+  
+  most_recent_onk_chapter_str = ""
+  
+  for i in range(0, len(most_recent_onk_chapter_array)):
+    most_recent_onk_chapter_str += most_recent_onk_chapter_array[i] + " "
+    
+  most_recent_onk_chapter_str = most_recent_onk_chapter_str.strip()
+  
+  last_onk_chapter = db_chapter.data_onk.find_one()
+  
+  if last_onk_chapter['title'] != most_recent_onk_chapter_str:
+      db_chapter.data_onk.find_one_and_update({'title':str(last_onk_chapter['title'])}, { '$set': { "title" : most_recent_onk_chapter_str} })
+      for channel in channels_onk.find():
+          if bot.get_channel((channel['id'])):
+            await (bot.get_channel(int(channel['id']))).send(f'{most_recent_onk_chapter_str} has been translated.\n{onk_chapter_anchor}, I suppose!')
+  
             
 # flip command        
 async def commands_flip(ctx):
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from commands.r_roll import commands_roll
 from commands.r_rps import commands_rps
 from commands.r_coinflip import commands_coinflip
 from commands.r_gif import commands_pat, commands_pout, commands_smug
-from commands.db.r_db import commands_add_channel, commands_remove_channel, tasks_check_chapter, tasks_filter_channels, commands_flip, commands_latest_chapter
+from commands.db.r_db import commands_add_channel, commands_remove_channel, commands_add_channel_kaguya, commands_remove_channel_kaguya, commands_add_channel_onk, commands_remove_channel_onk, tasks_check_chapter, tasks_filter_channels, commands_flip, commands_latest_chapter
 
 import threading
 
@@ -117,15 +117,35 @@ async def servers(ctx):
 async def avatar(ctx, member: discord.Member=None):
   await commands_avatar(ctx, member)
 
-# add channel to database
+# add channel to re zero notification list
 @bot.command(aliases = ["add"])
 async def add_channel(ctx):
   await commands_add_channel(ctx)
   
-# remove channel from database
+# remove channel from re zero notification list
 @bot.command(aliases = ["remove"])
 async def remove_channel(ctx):
   await commands_remove_channel(ctx)
+  
+# add channel to kaguya-sama notification list
+@bot.command(aliases = ["add_kaguya", "kaguya_add"])
+async def add_channel_kaguya(ctx):
+  await commands_add_channel_kaguya(ctx)
+  
+# remove channel from kaguya-sama notification list
+@bot.command(aliases = ["remove_kaguya", "kaguya_remove"])
+async def remove_channel_kaguya(ctx):
+  await commands_remove_channel_kaguya(ctx)
+  
+# add channel to oshi no ko notification list
+@bot.command(aliases = ["add_onk", "onk_add"])
+async def add_channel_onk(ctx):
+  await commands_add_channel_onk(ctx)
+  
+# remove channel from oshi no ko notification list
+@bot.command(aliases = ["remove_onk", "onk_remove"])
+async def remove_channel_onk(ctx):
+  await commands_remove_channel_onk(ctx)
   
 # help command
 @bot.command()
