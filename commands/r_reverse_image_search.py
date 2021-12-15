a08b70deab9fda5688b6265b432e995db5656fcd
Enes <eneskurbetoglu1@gmail.com>
2021-12-15 19:10:38 +0300

    added support for twitter sources if there are any
    now displaying two results one from pixiv one from twitter if they exist ofc

--- commands/r_reverse_image_search.py
+++ commands/r_reverse_image_search.py
@@ -1,5 +1,9 @@
 from dotenv import load_dotenv
-from pysaucenao import SauceNao
+from pysaucenao import SauceNao, PixivSource, GenericSource
+
+# no idea why but I can't import TwitterSource class on the line above
+import pysaucenao
+TwitterSource = pysaucenao.containers.TwitterSource
 
 import os
 import discord
@@ -8,7 +12,8 @@ load_dotenv()
 
 saucenao_api_key = os.getenv('SAUCENAO_API_KEY')
 
-saucenao = SauceNao(api_key=saucenao_api_key, results_limit=1, db=5)
+# add 'db' parameter for specific databases
+saucenao = SauceNao(api_key=saucenao_api_key)
 
 async def commands_reverse_image_search(ctx, url):
     try:
@@ -19,9 +24,28 @@ async def commands_reverse_image_search(ctx, url):
           color = discord.Colour.random()
         )
         results = await saucenao.from_url(url)
-        best_result = results[0]
-        sauce_frame.add_field(name=f"{best_result.title} by {best_result.author_name} with {str(best_result.similarity)} similarity", value=f"on [PIXIV]({best_result.source_url})")
-        sauce_frame.set_thumbnail(url=best_result.thumbnail)
+        pixiv_result = ""
+        twitter_result = ""
+        for result in results:
+            if isinstance(result, PixivSource):
+                pixiv_result = result
+            elif isinstance(result, TwitterSource):
+                twitter_result = result
+        if pixiv_result != "" and twitter_result != "":
+            sauce_frame.add_field(name=f"{pixiv_result.title} by {pixiv_result.author_name} with {str(pixiv_result.similarity)} similarity",\
+                             value=f"on [Pixiv]({pixiv_result.source_url}),\non [Twitter]({twitter_result.source_url})")
+            sauce_frame.set_thumbnail(url=pixiv_result.thumbnail)
+        elif pixiv_result == "" and twitter_result == "":
+            sauce_frame = discord.Embed(title="No results!", description=f"I couldn't find that on pixiv or twitter, I suppose!")
+        elif twitter_result == "":
+            sauce_frame.add_field(name=f"{pixiv_result.title} by {pixiv_result.author_name} with {str(pixiv_result.similarity)} similarity",\
+                             value=f"on [Pixiv]({pixiv_result.source_url})")
+            sauce_frame.set_thumbnail(url=pixiv_result.thumbnail)
+        else:
+            sauce_frame.add_field(name=f"Image posted by {twitter_result.author_name} with {str(twitter_result.similarity)} similarity",\
+                             value=f"on [Twitter]({twitter_result.source_url})")
+            sauce_frame.set_thumbnail(url=twitter_result.thumbnail)
+
         await ctx.send(embed=sauce_frame)
     except:
-        await ctx.send("I couldn't find that on pixiv, I suppose!'")
+        await ctx.send("It's not Betty's fault. Something went wrong, in fact!")
