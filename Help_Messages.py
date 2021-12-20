1f97393c38313febda3ef15729d140fed731e832
Enes <eneskurbetoglu1@gmail.com>
2021-12-20 21:33:35 +0300

    moved all the help string to a dictionary in a seperate file to get rid of all the if statements
    next up figure out how to add aliases to help commands

---
+++ Help_Messages.py
@@ -0,0 +1,72 @@
+messages = {
+        '': "```Prefix is 'r.', in fact!\
+                 \nTo get help and to learn aliases for a specific command use r.help <command>, I suppose!\
+                 \nAvailable commands are:\
+                 \n-add_channel\
+                 \n-remove_channel\
+                 \n-latest_chapter\
+                 \n-avatar\
+                 \n-say\
+                 \n-clean\
+                 \n-kick\
+                 \n-ban\
+                 \n-unban\
+                 \n-remind\
+                 \n-roll\
+                 \n-flip\
+                 \n-pout\
+                 \n-pat\
+                 \n-smug\
+                 \n-coinflip\
+                 \n-reverse_image_search\
+                 \n-rps, in fact!```",
+                 
+        'avatar': "```Betty will send a member's(author's if no member is passed) avatar image to the text channel, in fact!```",
+        
+        'add_channel': "```Betty will add the text channel to the list of channels that will receive notifications of the preferred series, I suppose!\
+                         \nAn alias for this is 'add', in fact!```",
+                         
+        'remove_channel': "```Betty will remove the text channel from the list of channels that will receiv notifications of the preferred series, I suppose!\
+                            \nAn alias for this is 'remove', in fact!```",
+                            
+        'say': "```Betty will repeat what you say, I suppose!```",
+        
+        'clean': "```Betty will clean up the text channel by the specified amount of messages, in fact!\
+                   \nAn alias for this is 'clear', in fact!```",
+                   
+        'kick': "```Betty will use wind magic to kick a member, I suppose!\
+                  \nAliases for this are 'yeet' and 'yeeto', in fact!```",
+                  
+        'ban': "```Betty will use yin magic to ban a member, I suppose!```",
+        
+        'unban': "```Betty will forgive a member and remove their ban, I suppose!```",
+        
+        'remind': "```Betty will remind the author after specified time by pinging them, I suppose!\
+                    \nThe format for this is 'remind time unit reason'\
+                    \n(The unit can be concatenated with remind such as remind 5m coffee, s: seconds, m: minutes, h: hours, d: days, ex. remind 5 m coffee), in fact!```",
+                    
+        'roll': "```Betty will roll a number for the author between 0 and the specified number(100 if not), I suppose!```",
+        
+        'flip': "```Betty doesn't want to talk about it, in fact!```",
+        
+        'pout': "```Betty will send pout gifs from tenor gif, I suppose!```",
+        
+        'pat': "```Pat a member or yourself(how pathetic, in fact!).```",
+        
+        'smug': "```Betty will send smug gifs from tenor gif, I suppose!```",
+        
+        'coinflip': "```Betty will flip a coin, I suppose!\
+                      \nAn alias for this is 'coin', in fact!\
+                      \nThe format for this is 'coinflip for_heads(not required) for_tails(not required)' (ex. coinflip lol valo), in fact!```",
+                      
+        'latest_chapter': "```Betty will tell you what the latest translated chapter is, I suppose!\
+                            \nAliases for this are 'chp', 'latest', and 'last' in fact!```",
+                            
+        'rps': "```Betty will play rock-paper-scissors with you, I suppose! Rejoice, in fact!\
+                 \nThe format for this is 'rps choice' (ex. rps rock), in fact!```",
+                 
+        'reverse_image_search': "```Betty will reverse search for the image you want, I suppose!\
+                                  \nAliases for this are 'ris', 'sauce', and 'source' in fact!\
+                                  \nThe format for this is 'reverse_image_search url', in fact!```",
+                                  
+}
