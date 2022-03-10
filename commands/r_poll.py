72b8db012104209a6e534d0648e55e1d142faabe
Enes <eneskurbetoglu1@gmail.com>
2022-03-10 20:50:47 +0300

    updated poll embed to display the question in the title

--- commands/r_poll.py
+++ commands/r_poll.py
@@ -6,11 +6,11 @@ from datetime import datetime
 
 async def commands_poll(ctx, c1, c2, question):
     embed = discord.Embed(
-        color=discord.Colour.random()
+        title=question.upper(),
+        description=f":one: {c1}\n\n:two: {c2}\n",
+        color=discord.Colour.random(),
     )
 
-    embed.add_field(name=f"{question.upper()}",
-                    value=f":one: {c1}\n\n:two: {c2}\n")
     embed.set_footer(
         text=f"Poll created by {ctx.author.nick}", icon_url=ctx.author.avatar_url)
 
@@ -23,16 +23,16 @@ async def commands_poll(ctx, c1, c2, question):
 
     await asyncio.sleep(180)
 
-    results = discord.Embed(
-        color=discord.Colour.random()
-    )
-
     message_1 = await ctx.channel.fetch_message(msg.id)
 
     reactions = {react.emoji: react.count for react in message_1.reactions}
 
-    results.add_field(
-        name=f"Results", value=f"{c1}: {reactions[emojis[0]]}\n\n{c2}: {reactions[emojis[1]]}")
+    results = discord.Embed(
+        title="Results", 
+        description=f"{c1}: {reactions[emojis[0]]}\n\n{c2}: {reactions[emojis[1]]}",
+        color=discord.Colour.random()
+    )
+    
     results.set_footer(
         text=f"For the poll '{question}'", icon_url=ctx.author.avatar_url)
 
