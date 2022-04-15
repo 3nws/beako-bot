6d5dd4b07b34bac21050fa3b9cf6f9c2065ec6a5
Enes <eneskurbetoglu1@gmail.com>
2022-04-15 10:28:11 +0300

    moved user requests in a function

--- cogs/Osu.py
+++ cogs/Osu.py
@@ -6,6 +6,7 @@ import asyncio
 from discord.ui import View, Select
 from discord.ext import commands
 from dotenv import load_dotenv
+from cogs.classes.Player import Player
 
 load_dotenv()
 
@@ -25,44 +26,50 @@ class Osu(commands.Cog):
             "2": "osu!catch",
             "3": "osu!mania",
         }
-
-    @commands.command(aliases=['u', 'user'])
-    async def osu(self, ctx, player=None):
-        mode = "0"
-        if player is None:
-            return await ctx.send("Who, in fact?!")
-        url = f"{self.base_url}get_user{self.key_query}&u={player}&m={mode}"
+        
+    async def get_user(self, username, mode):
+        url = f"{self.base_url}get_user{self.key_query}&u={username}&m={mode}"
         s = requests.session()
         r = s.get(url).json()[0]
-        username = r['username']
-        lvl = r['level'].split('.')[0]
-        progress = r['level'].split('.')[1][:2]+'%'
-        global_rank = r['pp_rank']
-        country_rank = r['pp_country_rank']
-        country = r['country']
-        pp = r['pp_raw'].split('.')[0]
-        acc = r['accuracy'][:5]
-        p_count = r['playcount']
-        p_time = str(int(r['total_seconds_played']) // 60 // 60)
-        ssh = r['count_rank_ssh']
-        sh = r['count_rank_sh']
-        ss = r['count_rank_ss']
-        s = r['count_rank_s']
-        a = r['count_rank_a']
-        game_mode = self.game_modes[mode]
-
-        desc = f"Rank: #{global_rank} (#{country_rank} {country})\n\n"
-        desc += f"{pp} pp, {acc}%, {p_count} plays ({p_time})\n\n"
-        desc += f"SSH: {ssh}, SH: {sh}, SS: {ss}, S: {s}, A: {a}"
+        player = Player()
+        player.userid = r['user_id']
+        player.username = r['username']
+        player.lvl = r['level'].split('.')[0]
+        player.progress = r['level'].split('.')[1][:2]+'%'
+        player.global_rank = r['pp_rank']
+        player.country_rank = r['pp_country_rank']
+        player.country = r['country']
+        player.pp = r['pp_raw'].split('.')[0]
+        player.acc = r['accuracy'][:5]
+        player.p_count = r['playcount']
+        player.p_time = str(int(r['total_seconds_played']) // 60 // 60)
+        player.ssh = r['count_rank_ssh']
+        player.sh = r['count_rank_sh']
+        player.ss = r['count_rank_ss']
+        player.s = r['count_rank_s']
+        player.a = r['count_rank_a']
         
+        player.desc = f"Rank: #{player.global_rank} (#{player.country_rank} {player.country})\n\n"
+        player.desc += f"{player.pp} pp, {player.acc}%, {player.p_count} plays ({player.p_time})\n\n"
+        player.desc += f"SSH: {player.ssh}, SH: {player.sh}, SS: {player.ss}, S: {player.s}, A: {player.a}"
+        
+        player.avatar_url = self.base_image_url+player.userid
+        return player
+
+    @commands.command(aliases=['u', 'user'])
+    async def osu(self, ctx, player_name=None):
+        mode = "0"
+        if player_name is None:
+            return await ctx.send("Who, in fact?!")
+        player = await self.get_user(player_name, mode)
+        game_mode = self.game_modes[mode]
         embed = discord.Embed(
             colour=discord.Colour.random(),
-            title=f"**{username} Lvl. {lvl} ({progress}) {game_mode}**",
-            url=f"{self.base_profile_url}{r['user_id']}",
-            description=desc,
+            title=f"**{player.username} Lvl. {player.lvl} ({player.progress}) {game_mode}**",
+            url=f"{self.base_profile_url}{player.userid}",
+            description=player.desc,
         )
-        avatar_url = self.base_image_url+r['user_id']
-        embed.set_thumbnail(url=avatar_url)
+        embed.set_thumbnail(url=player.avatar_url)
         options = [
                 discord.SelectOption(value=0, label="osu!"),
                 discord.SelectOption(value=1, label="osu!taiko"),
@@ -72,44 +79,28 @@ class Osu(commands.Cog):
         select = Select(options=options, placeholder="Select a game mode.")
         async def select_callback(i):
             mode = (i.data['values'])[0]
-            new_url = self.base_url+"get_user" + \
-                self.key_query+f"&u={player}&m="+mode
-            r = requests.get(new_url).json()[0]
-            lvl = r['level'].split('.')[0]
-            progress = r['level'].split('.')[1][:2]+'%'
-            global_rank = r['pp_rank']
-            country_rank = r['pp_country_rank']
-            country = r['country']
-            pp = r['pp_raw'].split('.')[0]
-            acc = r['accuracy'][:5]
-            p_count = r['playcount']
-            p_time = str(int(r['total_seconds_played']) // 60 // 60)
-            ssh = r['count_rank_ssh']
-            sh = r['count_rank_sh']
-            ss = r['count_rank_ss']
-            s = r['count_rank_s']
-            a = r['count_rank_a']
+            player = await self.get_user(player_name, mode)
             game_mode = self.game_modes[mode]
-
-            desc = f"Rank: #{global_rank} (#{country_rank} {country})\n\n"
-            desc += f"{pp} pp, {acc}%, {p_count} plays ({p_time})\n\n"
-            desc += f"SSH: {ssh}, SH: {sh}, SS: {ss}, S: {s}, A: {a}"
             new_embed = discord.Embed(
                 colour=discord.Colour.random(),
-                title=f"**{username} Lvl. {lvl} ({progress}) {game_mode}**",
-                url=f"{self.base_profile_url}{r['user_id']}",
-                description=desc,
+                title=f"**{player.username} Lvl. {player.lvl} ({player.progress}) {game_mode}**",
+                url=f"{self.base_profile_url}{player.userid}",
+                description=player.desc,
             )
-            new_embed.set_thumbnail(url=avatar_url)
+            new_embed.set_thumbnail(url=player.avatar_url)
             await i.response.edit_message(embed=new_embed)
         
         select.callback = select_callback
         view = View().add_item(select)
         msg = await ctx.send(embed=embed, view=view)
+        
+    # @commands.command(aliases=['rc', 'rs'])
+    # async def recent(self, ctx, player=None):
+    #     mode = "0"
+    #     if player is None:
+    #         return await ctx.send("Who, in fact?!")
+    #     url = f"{self.base_url}get_user_recent{self.key_query}&u={player}&m={mode}&limit=5"
 
-    @osu.error
-    async def osu_error(self, error, ctx):
-        print(error.cog)
 
 
 async def setup(bot):
---
+++ cogs/classes/Player.py
@@ -0,0 +1,21 @@
+class Player:
+    
+    def __init__(self):
+        self.userid = None
+        self.username = None
+        self.lvl = None
+        self.progress = None
+        self.global_rank = None
+        self.country_rank = None
+        self.country = None
+        self.pp = None
+        self.acc = None
+        self.p_count = None
+        self.p_time = None
+        self.ssh = None
+        self.sh = None
+        self.ss = None
+        self.s = None
+        self.a = None
+        self.desc = None
+        self.avatar_url = None
