f81810367024b11946c2ff11195e5c9af74d2985
Enes <eneskurbetoglu1@gmail.com>
2022-05-20 20:56:32 +0300

    cleaner clean command

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -21,7 +21,7 @@ class Admin(commands.Cog):
     # kicks member
     @app_commands.command(name="kick")
     @commands.has_permissions(kick_members=True)
-    async def kick(self, i: discord.Interaction, user: discord.Member, *, reason:str=None):
+    async def kick(self, i: discord.Interaction, user: discord.Member, *, reason: str = None):
         if user.top_role > i.user.top_role:
             return await i.response.send_message(f"You can't kick this person, I suppose!")
         await user.kick(reason=reason)
@@ -30,7 +30,7 @@ class Admin(commands.Cog):
     # bans member
     @app_commands.command(name="ban")
     @commands.has_permissions(ban_members=True)
-    async def ban(self, i: discord.Interaction, user: discord.Member, *, reason:str=None):
+    async def ban(self, i: discord.Interaction, user: discord.Member, *, reason: str = None):
         if user.top_role > i.user.top_role:
             return await i.response.send_message(f"You can't ban this person, I suppose!")
         await user.ban(reason=reason)
@@ -59,9 +59,9 @@ class Admin(commands.Cog):
             await ctx.send("You don't have permission to do that, I suppose!")
 
     async def clean_autocomplete(self,
-        interaction: discord.Interaction,
-        current: str,
-    ) -> List[app_commands.Choice[str]]:
+                                 interaction: discord.Interaction,
+                                 current: str,
+                                 ) -> List[app_commands.Choice[str]]:
         directions = ['Before', 'After']
         return [
             app_commands.Choice(name=direction, value=direction)
@@ -77,27 +77,17 @@ class Admin(commands.Cog):
         if direction is not None:
             direction = direction.lower()
         await i.response.defer()
+        original = await i.original_message()
         if (msg_id):
             msg = await i.channel.fetch_message(int(msg_id))
-            
             if direction == "after":
-                history = [message async for message in i.channel.history(limit=limit, after=msg, oldest_first=True) if message.interaction is None]
+                await i.channel.purge(limit=limit+1, bulk=True, after=msg, oldest_first=True, check=lambda m: m.id != original.id)
             elif direction == "before":
-                history = [message async for message in  i.channel.history(limit=limit, before=msg, oldest_first=False) if message.interaction is None]
-            to_delete = await i.channel.send("Cleaning, in fact!..")
-            for message in set(history):
-                await message.delete()
+                await i.channel.purge(limit=limit+1, bulk=True, before=msg, oldest_first=False, check=lambda m: m.id != original.id)
         elif (direction == "after"):
             return await i.response.send_message("I can't delete future messages, in fact! Tell me which message you want me to start deleting from, I suppose!")
-        elif (direction == "before"):
-            history = [message async for message in i.channel.history(limit=limit, before=ctx.message, oldest_first=False) if message.interaction is None]
-            to_delete = await i.channel.send("Cleaning, in fact!..")
-            for message in set(history):
-                await message.delete()
         else:
-            to_delete = await i.channel.send("Cleaning, in fact!..")
-            await i.channel.purge(limit=limit)
-        await to_delete.delete()
+            await i.channel.purge(limit=limit+1, bulk=True, check=lambda m: m.id != original.id)
         await i.followup.send(content='Cleared by {}, I suppose!'.format(i.user.mention))
 
     # deletes a member's all messages
