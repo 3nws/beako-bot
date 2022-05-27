22770abcc453c6990b1b73c271777dba143c88c1
Enes <eneskurbetoglu1@gmail.com>
2022-05-27 21:33:05 +0300

    deferring in translation now

--- cogs/TL.py
+++ cogs/TL.py
@@ -28,11 +28,12 @@ class TL(commands.Cog):
     @app_commands.autocomplete(language=lang_autocomplete)
     @app_commands.describe(text="The text you want to translate, in fact!", language="The language you want to translate to, I suppose!")
     async def translate(self, i: discord.Interaction, text: str, language: str):
+        await i.response.defer()
         detected = (await self.g.detect(text))[1].capitalize()
         lang_code = [s for s in async_google_trans_new.constant.LANGUAGES if async_google_trans_new.constant.LANGUAGES[s]==language][0]
         res = (await self.g.translate(text, lang_code)).strip()
         language = language.capitalize()
-        await i.response.send_message(f'"{text}" ({detected}) translates to "{res}" in {language}, in fact!')
+        await i.followup.send(f'"{text}" ({detected}) translates to "{res}" in {language}, in fact!')
         
 
 async def setup(bot: commands.Bot):
