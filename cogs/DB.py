9025cdd86afc29b1ed8f4fb23c7eb4deaa26a616
Enes <eneskurbetoglu1@gmail.com>
2022-05-27 15:40:48 +0300

    add command switched to buttons

--- cogs/DB.py
+++ cogs/DB.py
@@ -17,7 +17,8 @@ from commands.db.classes.MangaDex import MangaDex
 from discord import app_commands
 from discord.ext import commands
 from typing import List
-from .classes.Pagination import MangaReader, Source 
+from .classes.Pagination import MangaReader, Source
+from .classes.Pick import PickView
 
 
 class DB(commands.Cog):
@@ -85,13 +86,13 @@ class DB(commands.Cog):
         
         self.mangas_list = {}
         
-        self.tasks_change_avatar.start()
+    #     self.tasks_change_avatar.start()
         
         
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.tasks_filter_channels.start()
-        self.tasks_check_chapter.start()
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     self.tasks_filter_channels.start()
+    #     self.tasks_check_chapter.start()
 
         
     # flip command
@@ -408,71 +409,10 @@ class DB(commands.Cog):
             titles = msg[2]
             manga_ids = msg[3]
             msg = msg[1]
-            msg = await i.channel.send(embed=msg)
-            await i.response.send_message("Pick a series to follow, I suppose!")
+            manga_infos = (titles, manga_ids)
+            await i.response.send_message("Pick a series to follow, I suppose!", embed=msg, view=PickView(i, self.channels_md, manga_infos))
         else:
             return await i.response.send_message(msg)
-    
-        for j in range(len(manga_ids)):
-            await msg.add_reaction(emojis[j])
-
-        def check(reaction, user):
-            return user == i.user and reaction.message == msg
-
-        while True:
-            try:
-                reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=60.0)
-                channel_exists = True if await self.channels_md.find_one(
-                        {
-                        "channel_id": i.channel_id,
-                        "guild_id": i.guild.id,
-                        }
-                    ) else False
-                if not channel_exists:
-                    await self.channels_md.insert_one({
-                        'channel_id': i.channel_id,
-                        "guild_id": i.guild.id,
-                        'mangas': '{}',
-                    })
-                mangas_on_channel = (await self.channels_md.find_one(
-                    {
-                        "channel_id": i.channel_id,
-                        "guild_id": i.guild.id,
-                        }))['mangas']
-                mangas_dict = ast.literal_eval(mangas_on_channel)
-                idx = 0
-                for j, emoji in enumerate(emojis):
-                    if emoji == str(reaction):
-                        idx = j
-                        break
-                if str(reaction) == emojis[idx]:
-                    if manga_ids[idx] not in mangas_dict:
-                        chapter_response = await md.get_latest(manga_ids[idx])
-                        title_response = chapter_response.get_title()
-                        latest = title_response[0]
-                        is_title = title_response[1]
-                        chapter_link = chapter_response.get_link()
-                        mangas_dict.update({f"{manga_ids[idx]}": str(latest)})
-                        new_doc = await self.channels_md.find_one_and_update(
-                            {
-                                'channel_id': i.channel_id,
-                                "guild_id": i.guild.id,
-                                },
-                            {
-                                '$set': {
-                                    'mangas': str(mangas_dict)
-                                }
-                            },
-                            return_document=pymongo.ReturnDocument.AFTER
-                        )
-                        await i.channel.send(f"This channel will receive notifications on new chapters of {titles[idx]}, I suppose!")
-                    else:
-                        await i.channel.send(f"This channel is already on the receiver list for the new chapters of {titles[idx]}, I suppose!")
-            except asyncio.TimeoutError:
-                await msg.clear_reactions()
-                await msg.reply("I'm not accepting any follow requests anymore, in fact!")
-                break
-
 
 
     # remove the channel from the receiver list
--- cogs/classes/Pick.py
+++ cogs/classes/Pick.py
@@ -1,23 +1,104 @@
 import discord
 import asyncio
 import typing
+import pymongo
+import ast
 
 from discord import ui
+from commands.db.classes.MangaDex import MangaDex
 
-
-class Pick(ui.View):
+class PickView(ui.View):
     
-    def __init__(self):
+    def __init__(self, i: discord.Interaction, channels: pymongo.collection.Collection, info):
         super().__init__(timeout=60)
-        self._source = source
-        self.current_page = 0
-        self.i: discord.Interaction = None
-        self.msg: discord.Message = None
-        self.embed: discord.Embed = None
-        self.text: str = None
-        self.group: str = None
+        self.i: discord.Interaction = i
+        self.channels: pymongo.collection.Collection = channels
+        self.mangas: dict = {}
+        self.md: MangaDex = MangaDex()
+        self.info: tuple = info
+        self.num_of_results: int = len(self.info[0])
+        if self.num_of_results != len(self._children):
+            for j in range(len(self._children)-1, self.num_of_results-1, -1):
+                self.remove_item(self._children[j])
+        
         
     async def on_timeout(self):
         self.stop()
         await self.msg.edit(content=self.text, embed=self.embed, view=self.disabled())
         await self.msg.reply("This view just timed out, I suppose! You need to interact with it to keep it up, in fact!")
+        
+    async def interaction_check(self, interaction):
+        return interaction.user == self.i.user
+    
+    async def find_one(self):
+        channel_exist = await self.channels.find_one(
+                        {
+                        "channel_id": self.i.channel_id,
+                        "guild_id": self.i.guild.id,
+                        }
+                    )
+        if not channel_exist:
+            channel_exist = await self.channels.insert_one({
+                'channel_id': self.i.channel_id,
+                "guild_id": self.i.guild.id,
+                'mangas': '{}',
+            })
+            channel_exist = await self.channels.find_one(
+                        {
+                        "channel_id": self.i.channel_id,
+                        "guild_id": self.i.guild.id,
+                        }
+                    )
+        return ast.literal_eval(channel_exist['mangas'])
+    
+    async def update(self, choice: int):
+        res = await self.find_one()
+        titles = self.info[0]
+        manga_ids = self.info[1]
+        if manga_ids[choice] not in res:
+            chapter_response = await self.md.get_latest(manga_ids[choice])
+            title_response = chapter_response.get_title()
+            latest = title_response[0]
+            is_title = title_response[1]
+            chapter_link = chapter_response.get_link()
+            res.update({f"{manga_ids[choice]}": str(latest)})
+            new_doc = await self.channels.find_one_and_update(
+                {
+                    'channel_id': self.i.channel_id,
+                    "guild_id": self.i.guild.id,
+                    },
+                {
+                    '$set': {
+                        'mangas': str(res)
+                    }
+                },
+                return_document=pymongo.ReturnDocument.AFTER
+            )
+            await self.i.channel.send(f"This channel will receive notifications on new chapters of {titles[choice]}, I suppose!")
+        
+    @ui.button(emoji='1️⃣', style=discord.ButtonStyle.blurple)
+    async def opt_one(self, interaction, button):
+        await interaction.response.defer()
+        await self.update(0)
+        
+    @ui.button(emoji='2️⃣', style=discord.ButtonStyle.blurple)
+    async def opt_two(self, interaction, button):
+        await interaction.response.defer()
+        await self.update(1)
+        
+    @ui.button(emoji='3️⃣', style=discord.ButtonStyle.blurple)
+    async def opt_three(self, interaction, button):
+        await interaction.response.defer()
+        await self.update(2)
+        
+    @ui.button(emoji='4️⃣', style=discord.ButtonStyle.blurple)
+    async def opt_four(self, interaction, button):
+        await interaction.response.defer()
+        await self.update(3)
+        
+    @ui.button(emoji='5️⃣', style=discord.ButtonStyle.blurple)
+    async def opt_five(self, interaction, button):
+        await interaction.response.defer()
+        await self.update(4)
+        
+        
